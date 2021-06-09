ad1420591599bf3712f875c16dc1cc5a83c749dc
Alex Bertotto <ajb5206@gmail.com>
2021-06-08 19:01:36 -0700

    subtraction function

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+
+
+function add(number1, number2) {
+  return number1 + number2;
+  }
+  
+  const number1 = parseInt(prompt("Enter a number:"));
+  const number2 = parseInt(prompt("Enter another number:"));
+  alert(subtract(number1, number2));
+ 
